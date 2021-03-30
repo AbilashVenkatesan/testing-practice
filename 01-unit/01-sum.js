c9c5a666086e0f5ccc8c877236bfc41d4826b877
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-03-30 16:28:05 -0400

    Test case 4 - Addition & updated test case 3

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -26,12 +26,14 @@ try {
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-  var result = sum(4, undefined);
+  var result = sum(4, null);
   if (result !==4) throw new Error('Expected sum(4, 0) to be 4. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = sum(null, null);
+  if (result !==0) throw new Error('Expected sum(0, 0) to be 0. Received: ' + result);
   console.log('All tests passed successfully.');
 
 // ==================================================
