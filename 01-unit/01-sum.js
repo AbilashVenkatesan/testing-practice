1e2025c52766457bbc566e4ef04744e44ecf19aa
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-03-30 16:24:34 -0400

    Test case 3 - Addition

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -1,7 +1,7 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum(num1, num2) {
+function sum(num1, num2 = 0) {
 return num1 + num2;
 }
 
@@ -20,18 +20,18 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
-  var result = sum(1,2,3,4);
+  var result = sum(1, 2, 3, 4);
   if (result !==3) throw new Error('Expected sum(1, 2) to be 3. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = sum(4, undefined);
+  if (result !==4) throw new Error('Expected sum(4, 0) to be 4. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
   console.log('All tests passed successfully.');
 
 // ==================================================
